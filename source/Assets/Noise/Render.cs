d1241c8430be45826dc8a6de5320b0b3ec71b4ca
Masaoka <mikelito33bdx@gmail.com>
2020-04-11 23:00:16 +0900

    add project

---
+++ source/Assets/Noise/Render.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Render : MonoBehaviour
+{
+    [SerializeField] private Material mat = default;
+    
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(null, dest, mat);
+    }
+}
