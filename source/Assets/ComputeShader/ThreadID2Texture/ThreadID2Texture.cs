3f6945696f923b9f4c113018ff53fb36c22b4af9
Masaoka <mikelito33bdx@gmail.com>
2020-06-04 16:29:55 +0900

    texturebuffer

---
+++ source/Assets/ComputeShader/ThreadID2Texture/ThreadID2Texture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ThreadID2Texture : MonoBehaviour
+{
+
+    [SerializeField] private ComputeShader _computeShader;
+
+    [SerializeField] private GameObject _target;
+    
+    void Start()
+    {
+        var renderTexture = new RenderTexture(512,512,0, RenderTextureFormat.ARGB32);
+        renderTexture.enableRandomWrite = true;
+        renderTexture.Create();
+        
+        var kernelIndex = _computeShader.FindKernel("CSMain");
+        _computeShader.SetTexture(kernelIndex, "ResultBuffer", renderTexture);
+        _computeShader.Dispatch(kernelIndex, 64, 64 ,1);
+        
+        _target.GetComponent<Renderer>().material.mainTexture = renderTexture;
+    }
+    
+}
